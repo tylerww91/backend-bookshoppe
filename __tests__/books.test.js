3ed09ff0284b50ffb8ab11acb972fe56908d1c45
Tyler Watson <tylerww91@gmail.com>
2022-11-07 19:54:17 -0800

    adds test for /books route that should return a list of books. Removes test file that was made preemptively, builds app route in app.js. Starts route in controller

---
+++ __tests__/books.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('books routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET /books should return list of books', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(20);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(Number),
+      title: expect.any(String),
+      released: expect.any(String),
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
