547ad6c827c036d5d29faf3ffe28671a98a31272
Tyler Watson <tylerww91@gmail.com>
2022-11-08 16:33:32 -0800

    builds test for /authors/:id route

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -17,6 +17,24 @@ describe('authors routes', () => {
       name: expect.any(String),
     });
   });
+
+  it('GET /authors/:id should return an individual author and books written by them', async () => {
+    const resp = await request(app).get('/authors/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(10);
+    expect(resp.body[0]).toEqual({
+      name: expect.any(String),
+      date_of_birth: expect.any(String),
+      place_of_birth: expect.any(String),
+      books: [
+        {
+          id: expect.any(Number),
+          title: expect.any(String),
+          released: expect.any(Number),
+        },
+      ],
+    });
+  });
   afterAll(() => {
     pool.end();
   });
